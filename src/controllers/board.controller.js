6da22e987486846de52668ebdab2e47bb7bf7d6b
Tuan11ung <nguyenduytuan11ung@gmail.com>
2025-11-28 15:51:34 +0700

    refactor: improve code formatting and structure in board controller

--- src/controllers/board.controller.js
+++ src/controllers/board.controller.js
@@ -1,18 +1,29 @@
-import { StatusCodes } from "http-status-codes";
-import { boardService } from "~/services/board.service"
+import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/board.service'
 
 const createNew = async (req, res, next) => {
-    try {
-        //Dieu huong du lieu sang tang Service
-        const createdBoard = await boardService.creatNew(req.body)
+  try {
+    //Dieu huong du lieu sang tang Service
+    const createdBoard = await boardService.creatNew(req.body)
 
-        res.status(StatusCodes.CREATED).json(createdBoard)
-    } catch (error) {
-        next(error)
-    }
+    res.status(StatusCodes.CREATED).json(createdBoard)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const getDetails = async (req, res, next) {
+  try {
+    const boardId = req.params.id
+    const board = await boardService.getDetails(boardId)
+
+    res.status(StatusCodes.OK).json(board)
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const boardController = {
-    createNew
+  createNew
 }
 
